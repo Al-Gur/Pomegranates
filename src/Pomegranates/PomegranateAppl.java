9602b561ce0b7c1fe3e707d22d6f152cd741f180
Alexander <a@a>
2024-12-25 05:05:26 +0300

    Streams in new too

--- src/Pomegranates/PomegranateAppl.java
+++ src/Pomegranates/PomegranateAppl.java
@@ -8,43 +8,45 @@ public class PomegranateAppl {
         int nBoxes = new Random().nextInt(101) + 100;
         System.out.println("nBoxes = " + nBoxes);
         List<Box> boxes = new ArrayList<>();
-        for (int i = 0; i < nBoxes; i++) {
-            Box box = new Box();
-            List<Pomegranate> pomegranates = new ArrayList<>();
-            int nPomegranates = new Random().nextInt(11) + 10;
-            for (int j = 0; j < nPomegranates; j++) {
-                Pomegranate pomegranate = new Pomegranate();
-                List<Seed> seeds = new ArrayList<>();
-                int nSeeds = new Random().nextInt(301) + 400;
-                for (int k = 0; k < nSeeds; k++) {
-                    seeds.add(new Seed(2));
-                }
-                pomegranate.setSeeds(seeds);
-                pomegranates.add(pomegranate);
-            }
-            box.setGranates(pomegranates);
-            box.setName("Box " + i);
-            boxes.add(box);
-        }
+        new Random()
+                .ints(10, 21)
+                .limit(nBoxes)
+                .forEach(nPomegranatesInTheBox ->{
+                    Box box = new Box();
+                    List<Pomegranate> pomegranates = new ArrayList<>();
+                    new Random()
+                        .ints(400, 701)
+                        .limit(nPomegranatesInTheBox)
+                        .forEach(nSeedsInThePomegranate ->{
+                            Pomegranate pomegranate = new Pomegranate();
+                            List<Seed> seeds = new ArrayList<>();
+                            for (int k = 0; k < nSeedsInThePomegranate; k++) {
+                                seeds.add(new Seed(2));
+                            }
+                            pomegranate.setSeeds(seeds);
+                            pomegranates.add(pomegranate);
+                        });
+                    box.setGranates(pomegranates);
+                    box.setName("Box " + (boxes.size() +1) + " : " + pomegranates.size() + " pomegranates");
+                    boxes.add(box);
+                });
 
         long seedCount = boxes.stream()
-                .map(Box::getGranates)
-                .flatMap(Collection::stream)
-                .map(Pomegranate::getSeeds)
-                .flatMap(Collection::stream)
-                .count();
+                .mapToInt(box -> seedCount(box))
+                .sum();
         System.out.println("seedCount = " + seedCount);
 
         Map<Integer, List<Box>> countedBoxes = boxes.stream()
-                .collect(Collectors.groupingBy(box ->
-                                (int) ((Box) box).getGranates().stream()
-                                        .map(Pomegranate::getSeeds)
-                                        .flatMap(Collection::stream)
-                                        .count()
-                        )
-                );
+                .collect(Collectors.groupingBy(box -> seedCount((Box) box)));
         int maxSeedsInBox = Collections.max(countedBoxes.keySet());
         System.out.println("maxSeedsInBox = " + maxSeedsInBox);
         countedBoxes.get(maxSeedsInBox).forEach(box-> System.out.println(box.getName()));
+    }
+
+    private static int seedCount(Box box){
+        return (int) box.getGranates().stream()
+                .map(Pomegranate::getSeeds)
+                .flatMap(Collection::stream)
+                .count();
     }
 }
